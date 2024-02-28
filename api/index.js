035b4c67fa4f08da1d71732d18b41cf7edccea1e
Your Name <you@example.com>
2024-02-28 10:26:05 +0530

    create and run the server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app=express();
 
-app.listen(3001,() => {
-    console.log('server running on port 3001');
+app.listen(3000,() => {
+    console.log('server running on port 3000');
 });
