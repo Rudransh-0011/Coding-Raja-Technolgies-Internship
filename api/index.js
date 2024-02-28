ae7ee5566b8928a29385098e2b35a25b35fda8c9
Your Name <you@example.com>
2024-02-28 12:03:38 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
-const app=express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+mongoose.connect("mongodb+srv://rudranshb089:roni%401234@blogit.sitmk3u.mongodb.net/Coding-Raja-Technolgies-Internship?retryWrites=true&w=majority&appName=blogIt").then(
+    () => {
+        console.log('connected to mongodb');
+    }
+)
+.catch((err) =>{
+    console.log(err);
+}  );
+
+const app= express();
 
 app.listen(3000,() => {
     console.log('server running on port 3000');
